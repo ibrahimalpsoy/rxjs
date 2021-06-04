528d807537ca9197f8503ee4a7679dc1d0c5ec6d
ibrahimalpsoy <ibrahimalpsoyy@gmail.com>
2021-06-04 11:56:21 +0300

    .

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,30 +1,83 @@
-import { Component } from '@angular/core';
-import { defer, from, interval, of, range, timer } from "rxjs";
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { defer, from, fromEvent, interval, of, range, timer } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { debounceTime, distinct, distinctUntilChanged, filter, map } from "rxjs/operators";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'rxjs';
   subscription;
 
+  // @ViewChild("btn") //html içindeki referansı aldı. compile edildiğinde html tarafındaki elementi burdaki (myButton) ile eşleşiyor
+  // myButton: ElementRef
+
+  // ngAfterViewInit(): void {
+  //   fromEvent(this.myButton.nativeElement, "click").subscribe(data => {
+  //     console.log(data);
+  //   });
+  // }
+
+  @ViewChild("txtSearch")
+  myText: ElementRef
+
+
+  ngAfterViewInit(): void {
+    fromEvent<KeyboardEvent>(this.myText.nativeElement, "keyup")
+      .pipe(debounceTime(500),)
+      .subscribe(data => { console.log((data.target as HTMLInputElement).value); });
+  }
+
+
   constructor() {
 
-    //this.ofOperation(); // içindeki herneyse bunu observable olarak döner
-    //this.intervalOperator();
-    //this.timerOprationAndunsubcribe();
+    //this.ofOperation(); // içindeki herneyse bunu observable olarak döner 
+    //this.intervalOperator(); 
+    //this.timerOprationAndunsubcribe(); 
     //this.rangeOperator(); 
     //this.fromOperator();
     //this.deferOparator();
+    //this.ajaxOparetor();
+    //this.filterOparator();
+    //this.distictUntilChangeOparator();
+    const numbers = from([1, 2, 3, 4, 54, 6]);
 
+    numbers.pipe(map(data => data + 2)).subscribe(data => console.log(data))
 
 
   }
 
+
+  private distictUntilChangeOparator() {
+    const distinctUntilOpr = from([1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 10]);
+
+    distinctUntilOpr.pipe(distinctUntilChanged()).subscribe(data => {
+
+      console.log(data);
+    });
+  }
+
+  private filterOparator() {
+    const myArray = from([1, 2, 3, 4, 5, 6, 7, 88, 99, 11, 22, 333, 4456]);
+
+    myArray.pipe(filter(val => val > 7)).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  private ajaxOparetor() {
+    ajax.getJSON("https://jsonplaceholder.typicode.com/users").subscribe(data => {
+      // console.log(`${data[0].name}`);
+      console.table(data);
+    }, err => { }, () => {
+      console.log("veriler geldi iyimi");
+    });
+  }
+
   private deferOparator() {
-    
+
     const deferOpr = of(new Date()); // subscribe olunduğunda oluşturulduğu andaki tarihi verir
     const deferOpr2 = defer(() => of(new Date())); // ne zaman çağırılırsa ozamanki tarihi verir.
 
@@ -75,7 +128,7 @@ export class AppComponent {
   }
 
   stopTimer() {
-    this.subscription.unsubscribe();
+    //this.subscription.unsubscribe();
   }
 
   private intervalOperator() {
